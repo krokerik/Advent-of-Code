5663af1a7dc2707e1543c254e2e33ab1ee8861bc
Erik Andersson <dv13ean@cs.umu.se>
2016-12-01 16:19:34 +0100

    solved second star

--- 2016/c/Dec01.c
+++ 2016/c/Dec01.c
@@ -3,12 +3,17 @@
 #include <string.h>
 
 #define INPUT "../input/01.txt"
+#define DELIM ", "
 #define NORTH 0
 #define EAST 1
 #define SOUTH 2
 #define WEST 3
 
-int getNumBlocks(char* instructions);
+typedef struct pos {
+	int north, east;
+}pos;
+
+void traverse(char* instructions);
 
 int main() {
 	FILE * fp;
@@ -21,28 +26,33 @@ int main() {
 		exit(EXIT_FAILURE);
 	}
 
-	while (getline(&line, &len, fp) != -1) {
-		printf("num blocks: %d\n", getNumBlocks(line));
-	}
+	while (getline(&line, &len, fp) != -1) traverse(line);
 
 	fclose(fp);
 	free(line);
 	exit(EXIT_SUCCESS);
 }
 
-int getNumBlocks(char* instructions) {
-	const char delim[3] = ", ";
-	char *instruction;
+void traverse(char* instructions) {
+	char* instruction;
 	char direction;
-	int distance, facing;
-	int total[4];
+	int distance, facing, total[2], histLen, looped;
+	pos* history;
 	
 	facing = NORTH;
-	instruction = strtok(instructions, delim);
+	
+	instruction = strtok(instructions, DELIM);
+	
 	total[NORTH] = 0;
-	total[EAST] = 0;
-	total[SOUTH] = 0;
-	total[WEST] = 0;
+	total[EAST]  = 0;
+	
+	histLen = 1;
+	
+	looped = 0;
+	
+	history = malloc(sizeof(pos*) * histLen);
+	history[0].east  = 0;
+	history[0].north = 0;
 	
 	while(instruction != NULL) {
 		distance = atoi(instruction+1);
@@ -56,7 +66,8 @@ int getNumBlocks(char* instructions) {
 				facing++;
 				break;
 			default:
-				fprintf(stderr, "Invalid direction '%c'\n", direction);
+				fprintf(stderr, "Invalid direction '%c' in instruction '%s'\n",
+				        direction, instruction);
 				exit(EXIT_FAILURE);
 		}
 		if(facing<NORTH) {
@@ -64,11 +75,48 @@ int getNumBlocks(char* instructions) {
 		} else if(facing>WEST) {
 			facing = NORTH;
 		}
+
+		pos position;
 		
-		total[facing] += distance;
+		for(int i=0; i<distance; i++) {
+			switch(facing) {
+				case NORTH:
+					total[NORTH] += 1;
+					break;
+				case EAST:
+					total[EAST]  += 1;
+					break;
+				case SOUTH:
+					total[NORTH] -= 1;
+					break;
+				case WEST:
+					total[EAST]  -= 1;
+					break;
+				default:
+					fprintf(stderr,"Invalid direction '%d'\n", facing);
+					exit(EXIT_FAILURE);
+			}
+			position.north = total[NORTH];
+			position.east  = total[EAST];
+
+			for(int i=0; i<histLen && looped == 0; i++) {
+				if(history[i].north==position.north && 
+				   history[i].east==position.east) {
+					printf("loop at (%d;%d), distance: %d\n",position.north,
+					       position.east,
+						   abs(position.north)+abs(position.east));
+					looped = 1;
+				}
+			}
+			history = realloc(history, sizeof(pos) * ++histLen);
+			history[histLen-1] = position;
+		}
 		
-		instruction = strtok(NULL, delim);
+		instruction = strtok(NULL, DELIM);
 	}
+	free(history);
 	
-	return abs(total[NORTH]-total[SOUTH])+abs(total[EAST]-total[WEST]);
+	if(looped == 0) printf("no loop found\n");
+	printf("num blocks total distance: %d\n",
+	       abs(total[NORTH])+abs(total[EAST]));
 }
