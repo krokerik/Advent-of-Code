2b59d995bdbc9a61af454c28b2a81cfd10e79b0e
Erik Andersson <dv13ean@cs.umu.se>
2017-12-01 13:31:56 +0100

    solved part1

--- 2017/c/Dec01.c
+++ 2017/c/Dec01.c
@@ -4,6 +4,8 @@
 
 #define INPUT "../input/01.txt"
 
+int captcha(char* seq);
+
 int main() {
 	FILE * fp;
 	char * line = NULL;
@@ -16,10 +18,26 @@ int main() {
 	}
 
 	while (getline(&line, &len, fp) != -1){
-
+		printf("%s%d\n\n", line, captcha(line));
 	}
 
 	fclose(fp);
 	free(line);
 	exit(EXIT_SUCCESS);
+}
+
+int captcha(char* seq) {
+	int res = 0, search = 0;
+	for(int i=0; i<strlen(seq); i++) {
+		int cur = seq[i] - '0';
+		if(cur < 1 || cur > 9) continue; //ignore non-digits
+		if(cur==search) {
+			res+=cur;
+		}
+		search = cur;
+	}
+	if(seq[0]-'0'==search) { //check circular loop.
+		res+=search;
+	}
+	return res;
 }
