afc2f1429c1921a40d97818190721d277653ecd8
Erik Andersson <dv13ean@cs.umu.se>
2017-12-01 13:48:11 +0100

    solved part 2

--- 2017/c/Dec01.c
+++ 2017/c/Dec01.c
@@ -4,7 +4,8 @@
 
 #define INPUT "../input/01.txt"
 
-int captcha(char* seq);
+int part1(char* seq);
+int part2(char* seq);
 
 int main() {
 	FILE * fp;
@@ -18,7 +19,7 @@ int main() {
 	}
 
 	while (getline(&line, &len, fp) != -1){
-		printf("%s%d\n\n", line, captcha(line));
+		printf("%s%d %d\n\n", line, part1(line), part2(line));
 	}
 
 	fclose(fp);
@@ -26,7 +27,7 @@ int main() {
 	exit(EXIT_SUCCESS);
 }
 
-int captcha(char* seq) {
+int part1(char* seq) {
 	int res = 0, search = 0;
 	for(int i=0; i<strlen(seq); i++) {
 		int cur = seq[i] - '0';
@@ -39,5 +40,18 @@ int captcha(char* seq) {
 	if(seq[0]-'0'==search) { //check circular loop.
 		res+=search;
 	}
+	return res;
+}
+
+// breaks if input doesn't end with a line feed
+int part2(char* seq) {
+	int res = 0, search = 0, len = strlen(seq)-2 , jump = len/2;
+	for(int i=0; i<len; i++) {
+		int pos = (i+jump)%len;
+		if(seq[i] == seq[pos]) {
+			res += seq[i]-'0';
+		}
+	}
+
 	return res;
 }
