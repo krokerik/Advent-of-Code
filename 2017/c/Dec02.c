52e5f9572d0306f0970d642354f952baba7d89c6
Erik Andersson <dv13ean@cs.umu.se>
2017-12-02 13:20:08 +0100

    part 2 solved

--- 2017/c/Dec02.c
+++ 2017/c/Dec02.c
@@ -6,7 +6,7 @@
 #define INPUT "../input/02.txt"
 
 long part1(char* seq);
-int part2(char* seq);
+long part2(char* seq);
 
 int main() {
 	FILE * fp;
@@ -20,14 +20,14 @@ int main() {
 	}
 
 	long tot1 = 0;
-	int tot2 = 0;
+	long tot2 = 0;
 	while (getline(&line, &len, fp) != -1){
 		line[strcspn(line, "\r\n")] = 0; //remove line breaks.
 		tot1+=part1(line);
 		tot2+=part2(line);
 	}
 	printf("part1: %ld\n",tot1);
-	printf("part2: %d\n",tot2);
+	printf("part2: %ld\n",tot2);
 
 	fclose(fp);
 	free(line);
@@ -51,6 +51,27 @@ long part1(char* seq) {
 	return biggest - smallest;
 }
 
-int part2(char* seq) {
-	return 0;
+long part2(char* seq) {
+	long dividend = -1, divisor = -1, *list = malloc(sizeof(long)), pos = 0;
+	char* end;
+	for (long i = strtol(seq, &end, 10); seq != end; i = strtol(seq, &end, 10)){
+		list[pos++] = i;
+		list = realloc(list,sizeof(long)*(pos+1));
+		seq = end;
+	}
+	for(int i = 0; i<pos; i++) {
+		for(int j = 0; j<pos; j++) {
+			if(i!=j && list[i]%list[j]==0) {
+				dividend = list[i];
+				divisor  = list[j];
+				break;
+			}
+		}
+		if(dividend != -1) {
+			break;
+		}
+	}
+	
+	free(list);
+	return dividend/divisor;
 }
