6415b42cc36b63dc34b79e84c6ea06088a305356
Erik Andersson <dv13ean@cs.umu.se>
2017-12-04 13:55:48 +0100

    ugly solution for part 2

--- 2017/c/Dec03.c
+++ 2017/c/Dec03.c
@@ -24,7 +24,7 @@ int main() {
 	while (getline(&line, &len, fp) != -1){
 		line[strcspn(line, "\r\n")] = 0; //remove line breaks.
 		printf("%s: %ld\n",line,part1(line));
-		//printf("%s: %ld\n",line,part2(line));
+		printf("%s: %ld\n",line,part2(line));
 	}
 
 	fclose(fp);
@@ -38,7 +38,7 @@ long part1(char* seq) {
 	     total = 0,
 	     width = 0;
 	
-	int i=1, lvl; 
+	int i=1, lvl;
 	do {
 		lvl = 4*pow(i,2) - 4*i + 1;
 		i++;
@@ -62,7 +62,71 @@ long part1(char* seq) {
 }
 
 long part2(char* seq) {
-	return 0;
+	char* end;
+	long input = strtol(seq,&end,10),
+	     total = 0,
+	     width = 0;
+	
+	int i=1, lvl;
+	do {
+		lvl = 4*pow(i,2) - 4*i + 1;
+		i++;
+	} while(lvl < input);
+	i-=2;
+	width = (i*2)+1;
+	int** spiral = (int **)malloc(width * sizeof(int *));
+	int x = floor(width/2), y = floor(width/2);
+	int dx = 1, dy = 0;
+	int level = 0;
+	
+	for(int i=0; i<width; i++) {
+		spiral[i] = malloc(width * sizeof(int));
+		for(int j=0; j<width; j++) {
+			spiral[i][j] = -1;
+		}
+	}
+	for(int i=1; i<=width*width; i++) {
+		if(i==1) {
+			spiral[y][x] = 1;
+		} else {
+			long sum = 0;
+			for(int j = y-1; j <= y+1; j++) {
+				if(j>=0 && j<width) {
+					for(int k = x-1; k <= x+1; k++) {
+						if(k>=0 && k<width && spiral[j][k]!=-1) {
+							sum += spiral[j][k];
+						}
+					}
+				}
+			}
+			spiral[y][x] = sum;
+			if(sum > input) {
+				total = sum;
+				break;
+			}
+		}
+		x += dx;
+		y += dy;
+		if(sqrt(i) == floor(sqrt(i)) && ((int)floor(sqrt(i)))%2==1) {
+			//bottom right corner.
+			level++;
+			dx = 0;
+			dy = -1;
+		} else if(i == 4*pow(level,2) - 2*level) {
+			//top right corner.
+			dx = -1;
+			dy = 0;
+		} else if(i == 4*pow(level,2)) {
+			//top left corner.
+			dx = 0;
+			dy = 1;
+		} else if(i == 4*pow(level,2) + 2*level) {
+			//bottom left corner.
+			dx = 1;
+			dy = 0;
+		}
+	}
+	return total;
 }
 
 int** getSpiral(int width) {
