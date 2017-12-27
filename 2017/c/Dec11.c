aeec7b4c53cde42a85f688ba9dfed125841f34a4
Erik Andersson <dv13ean@cs.umu.se>
2017-12-27 14:13:58 +0100

    mended my part 1 solution to work with part 2

--- 2017/c/Dec11.c
+++ 2017/c/Dec11.c
@@ -5,55 +5,116 @@
 
 #define INPUT "../input/11.txt"
 
+int getDistance(int dx, int dy, int dz);
+
 int main() {
-	FILE * fp;
-	char * line = NULL;
-	size_t len = 0;
+    FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
 
-	fp = fopen(INPUT, "r");
-	if (fp == NULL) {
-		perror(INPUT);
-		exit(EXIT_FAILURE);
-	}
-		
-	while (getline(&line, &len, fp) != -1){
-		line[strcspn(line, "\r\n")] = 0; //remove line breaks.
-		int dx  = 0,
-		    dy  = 0,
-		    dz  = 0,
-		    max = 0;
-		char *token;
-		
-		token = strtok(line, ",");
-		while(token!=NULL) {
-			if(strcmp(token,"n") == 0){
-				dy++;
-				dz--;
-			} else if(strcmp(token,"ne") == 0){
-				dx++;
-				dz--;
-			} else if(strcmp(token,"se") == 0){
-				dx++;
-				dy--;
-			} else if(strcmp(token,"s") == 0){
-				dz++;
-				dy--;
-			} else if(strcmp(token,"sw") == 0){
-				dz++;
-				dx--;
-			} else if(strcmp(token,"nw") == 0){
-				dy++;
-				dx--;
-			}
-			int tmp = (abs(dx)+abs(dy)+abs(dz))/2;
-			max = tmp>max?tmp:max;
-			token = strtok(NULL, ",");
-		}
-		printf("part1: %d\n",(abs(dx)+abs(dy)+abs(dz))/2);
-		printf("part2: %d\n",max);
-	}
+    fp = fopen(INPUT, "r");
+    if (fp == NULL) {
+        perror(INPUT);
+        exit(EXIT_FAILURE);
+    }
 
-	fclose(fp);
-	free(line);
-	exit(EXIT_SUCCESS);
+    while (getline(&line, &len, fp) != -1){
+        line[strcspn(line, "\r\n")] = 0; //remove line breaks.
+        int dx  = 0,
+            dy  = 0,
+            dz  = 0,
+            max = 0;
+        char *token;
+
+        token = strtok(line, ",");
+        while(token!=NULL) {
+            if(strcmp(token,"n") == 0){
+                dz++;
+            } else if(strcmp(token,"ne") == 0){
+                dx++;
+            } else if(strcmp(token,"se") == 0){
+                dy++;
+            } else if(strcmp(token,"s") == 0){
+                dz--;
+            } else if(strcmp(token,"sw") == 0){
+                dx--;
+            } else if(strcmp(token,"nw") == 0){
+                dy--;
+            }
+            int tmp = getDistance(dx,dy,dz);
+            //printf("dist: %d\n",tmp);
+            max = tmp>max?tmp:max;
+            token = strtok(NULL, ",");
+        }
+        printf("part1: %d\n",getDistance(dx,dy,dz));
+        printf("part2: %d\n",max);
+    }
+
+    fclose(fp);
+    free(line);
+    exit(EXIT_SUCCESS);
+}
+
+int getDistance(int dx, int dy, int dz) {
+    /*
+    x = ne-sw
+    y = se-nw
+    z =  n-s
+
+    n       = +dz
+    ne      = +dx
+    se      = +dy
+    s       = -dz
+    sw      = -dx
+    nw      = -dy
+
+    n  + sw = nw => dz>0, dx>0, dy--
+    s  + ne = se => dz<0, dx>0, dy++
+    s  + nw = sw => dz<0, dy<0, dx--
+    n  + se = ne => dz>0, dy>0, dx++
+    se + sw = s  => dy>0, dx<0, dz--
+    ne + nw = n  => dx>0, dy<0, dz++
+
+    */
+    int changed = 1;
+    while(changed == 1) {
+        changed = 0;
+        if(dz>0 && dx<0) {
+            dz--;
+            dx++;
+            dy--;
+            changed = 1;
+        }
+        if(dz>0 && dy>0) {
+            dz--;
+            dy--;
+            dx++;
+            changed = 1;
+        }
+        if(dz<0 && dy<0) {
+            dz++;
+            dy++;
+            dx--;
+            changed = 1;
+        }
+        if(dz<0 && dx>0) {
+            dz++;
+            dx--;
+            dy++;
+            changed = 1;
+        }
+        if(dy>0 && dx<0) {
+            dy--;
+            dx++;
+            dz--;
+            changed = 1;
+        }
+        if(dx>0 && dy<0) {
+            dx--;
+            dy++;
+            dz++;
+            changed = 1;
+        }
+    }
+    return abs(dx) + abs(dy) + abs(dz);
 }
