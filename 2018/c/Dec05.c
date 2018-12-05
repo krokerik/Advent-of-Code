1545f6a565571b5d0302c12af8480e06f02c91fc
Erik Andersson <dv13ean@cs.umu.se>
2018-12-05 17:20:01 +0100

    threaded solution, now runs in 0.7s

--- 2018/c/Dec05.c
+++ 2018/c/Dec05.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 
 #define INPUT "../input/05.txt"
 
+typedef struct thread_args {
+	char* polymer;
+	char unit;
+} thread_args;
+
+void* part1(void* arg);
+void* part2(void* arg);
 char* react(char* polymer);
 char changePolarity(char unit);
 void removeUnit(char* str, char unit);
+
+int shortest;
+pthread_mutex_t shortest_lock;
 
 int main() {
 	FILE * fp;
@@ -19,29 +30,58 @@ int main() {
 		exit(EXIT_FAILURE);
 	}
 
+	pthread_mutex_init(&shortest_lock,NULL);
 	while (getline(&line, &len, fp) != -1){
 		line[strcspn(line, "\r\n")] = 0; //remove line breaks.
-		char * input = malloc(sizeof(char)*len);
-		strcpy(input, line);
-		printf("part1: %zu\n",strlen(react(input)));
 
-		strcpy(input, line);
-		int shortest = len;
+		pthread_t part1_t;
+		pthread_attr_t attr;
+		pthread_attr_init(&attr);
+		pthread_create(&part1_t,&attr,part1,line);
+		pthread_join(part1_t,NULL);
+
+		shortest = len;
+		pthread_t part2_t[26];
 		for(char c = 'a'; c<='z'; c++) {
-			removeUnit(input,c);
-			int length = strlen(react(input));
-			if(length < shortest) {
-				shortest = length;
-			}
-			strcpy(input, line);
+			pthread_attr_t attributes;
+			pthread_attr_init(&attributes);
+			thread_args* arg = malloc(sizeof(thread_args));
+			arg->polymer = line;
+			arg->unit = c;
+			pthread_create(&part2_t[c-'a'],&attr,part2,arg);
 		}
+		for(int i=0; i<26; i++) {
+			pthread_join(part2_t[i],NULL);
+		}
+
 		printf("part2: %d\n",shortest);
-		free(input);
 	}
 
+	pthread_mutex_destroy(&shortest_lock);
 	fclose(fp);
 	free(line);
 	exit(EXIT_SUCCESS);
+}
+
+void* part1(void * arg) {
+	char* polymer =  arg;
+	printf("part1: %zu\n",strlen(react(polymer)));
+	pthread_exit(0);
+}
+void* part2(void* arg) {
+	thread_args* args = arg;
+	char* polymer = malloc(strlen(args->polymer)+1);
+	strcpy(polymer,args->polymer);
+	removeUnit(polymer,args->unit);
+	int len = strlen(react(polymer));
+	pthread_mutex_lock(&shortest_lock);
+	if(len<shortest) {
+		shortest = len;
+	}
+	pthread_mutex_unlock(&shortest_lock);
+	free(polymer);
+	free(args);
+	pthread_exit(0);
 }
 
 char* react(char* polymer) {
